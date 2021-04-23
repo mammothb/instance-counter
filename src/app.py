af6be1fa4d3da44400e6cdd0f3ad4ae32565f57c
mmb L <mammothb+git@gmail.com>
2021-04-23 10:37:20 +0800

    added media player

--- src/app.py
+++ src/app.py
@@ -1,5 +1,8 @@
 import sys
 
+from PyQt5.QtCore import QUrl
+from PyQt5.QtMultimedia import QMediaContent, QMediaPlayer
+from PyQt5.QtMultimediaWidgets import QVideoWidget
 from PyQt5.QtWidgets import (
     QApplication,
     QFileDialog,
@@ -8,6 +11,7 @@ from PyQt5.QtWidgets import (
     QLineEdit,
     QMainWindow,
     QPushButton,
+    QStyle,
     QWidget,
 )
 
@@ -17,10 +21,11 @@ class LayoutDialog(QMainWindow):
         super().__init__()
         self.left = 300
         self.top = 300
-        self.width = 400
-        self.height = 450
+        self.width = 640
+        self.height = 480
 
         # self.work = WorkThread()
+        self.media_player = QMediaPlayer(None, QMediaPlayer.VideoSurface)
         self.init_widgets().init_layout().init_event()
 
     def init_widgets(self):
@@ -35,6 +40,10 @@ class LayoutDialog(QMainWindow):
         # self.movie_source_combobox.addItem(self.tr('电影天堂'))
 
         self.button_process = QPushButton(self.tr("Process"))
+        self.video_widget = QVideoWidget()
+        self.button_play = QPushButton()
+        self.button_play.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.button_play.setEnabled(False)
 
         # self.tip_label = QLabel(self.tr("未开始查询..."))
         # self.search_content_label = QLabel(self.tr("查询内容:"))
@@ -51,19 +60,37 @@ class LayoutDialog(QMainWindow):
         layout.addWidget(self.button_browse, 0, 2)
         layout.addWidget(self.button_process, 0, 3)
 
+        layout.addWidget(self.video_widget, 1, 0, 2, 4)
+        layout.addWidget(self.button_play, 3, 0)
+
         frame = QWidget()
         self.setCentralWidget(frame)
         frame.setLayout(layout)
+
+        self.media_player.setVideoOutput(self.video_widget)
+
         return self
 
     def init_event(self):
         self.button_browse.clicked.connect(self.browse_file)
+        self.button_play.clicked.connect(self.play)
 
     def browse_file(self):
-        filename = QFileDialog.getOpenFileName(
-            self, "Open file", "C:\\", "Video files (*.mp4)"
+        filename, _ = QFileDialog.getOpenFileName(
+            self, "Open file", "C:\\", "Video files (*.mp4 *.avi)"
         )
-        self.line_edit_filename.setText(filename[0])
+        if filename != "":
+            self.line_edit_filename.setText(filename)
+            self.media_player.setMedia(
+                QMediaContent(QUrl.fromLocalFile(filename))
+            )
+            self.button_play.setEnabled(True)
+
+    def play(self):
+        if self.media_player.state() == QMediaPlayer.PlayingState:
+            self.media_player.pause()
+        else:
+            self.media_player.play()
 
 app = QApplication(sys.argv)
 dialog = LayoutDialog()
